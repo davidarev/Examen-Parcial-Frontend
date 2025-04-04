f12d0215128233468810eb1bba2367be6edb3d34
David Arévalo <72552800+davidarev@users.noreply.github.com>
2025-04-04 17:08:23 +0200

    Create Header.tsx

---
+++ componentes/Header.tsx
@@ -0,0 +1,11 @@
+import { FunctionalComponent } from "preact/src/index.d.ts";
+
+const Header: FunctionalComponent = () => {
+    return (
+        <>
+            <p>Esto es un header</p>
+        </>
+    )
+}
+
+export default Header;
